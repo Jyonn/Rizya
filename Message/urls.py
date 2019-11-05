3b7e8f8f41b4f03aedf62ac2ede19aa407727d27
lqj679ssn <lqj679ssn@qq.com>
2019-11-05 20:50:22 +0800

    重新开始Rizya

--- Message/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from Message.views import BaseView
-
-urlpatterns = [
-    path('', BaseView.as_view()),
-]
--- Trigger/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TriggerConfig(AppConfig):
-    name = 'Trigger'
