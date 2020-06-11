3e3a5a90b86b9894ba3a1a64106ea3e8fad8dd08
lqj679ssn <lqj679ssn@qq.com>
2020-06-11 18:35:15 +0800

    debug

--- Space/views.py
+++ Space/views.py
@@ -91,8 +91,11 @@ class TicketView(View):
     @Auth.require_login
     def put(r):
         """进入星球"""
+        print('in space')
         space = r.d.spaceman.space
+        print('checker')
         space.not_member_checker(r.user)
+        print('add member')
         space.add_member(r.user)
         return 0
 
